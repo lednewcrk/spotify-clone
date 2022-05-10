b7856415ff357231efcf5bd5741e6bf997ec85b0
Wendel <rwendelpereira@gmail.com>
2022-05-09 23:28:23 -0300

    feat: Create Dot component

---
+++ src/components/Dot/index.tsx
@@ -0,0 +1,21 @@
+import { Theme } from '@config/theme';
+import React from 'react';
+import { View, ViewStyle } from 'react-native';
+
+import { styles } from './styles';
+
+export interface DotProps {
+  color?: string;
+  size?: number;
+}
+
+export function Dot({ color = Theme.COLORS.SECONDARY, size = 10 }: DotProps) {
+  const style: ViewStyle = {
+    backgroundColor: color,
+    width: size,
+    height: size,
+    borderRadius: size / 2,
+  };
+
+  return <View style={[styles.container, style]} />;
+}
