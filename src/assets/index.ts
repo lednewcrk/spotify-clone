8d9792d819d939cec43c4712f1cebfc852983c63
Wendel <rwendelpereira@gmail.com>
2022-05-12 00:36:07 -0300

    refactor: Rename library icon

---
+++ src/assets/index.ts
@@ -0,0 +1,15 @@
+export const ICONS = {
+  BELL: require('./icons/bell.png'),
+  CLOCK: require('./icons/clock.png'),
+  GEAR: require('./icons/gear.png'),
+  HOME: require('./icons/home.png'),
+  HOME_OUTLINE: require('./icons/home-outline.png'),
+  LIBRARY: require('./icons/library.png'),
+  LIBRARY_OUTLINE: require('./icons/library-outline.png'),
+  SEARCH: require('./icons/search.png'),
+  SEARCH_OUTLINE: require('./icons/search-outline.png'),
+};
+
+export const LOTTIES = {
+  WAVES: require('./lotties/waves.json'),
+};
