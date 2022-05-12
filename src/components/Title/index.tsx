34668432ebf59a5713ae9f70a413fc1935e1f0c6
Wendel <rwendelpereira@gmail.com>
2022-05-12 00:37:09 -0300

    refactor: Title is able to receive title by props

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -3,8 +3,14 @@ import { Text, TextProps } from 'react-native';
 
 import { styles } from './styles';
 
-export type TitleProps = TextProps;
+export type TitleProps = TextProps & {
+  text?: string;
+};
 
-export function Title({ style, ...rest }: TitleProps) {
-  return <Text style={[styles.title, style]} {...rest} />;
+export function Title({ style, children, text, ...rest }: TitleProps) {
+  return (
+    <Text style={[styles.title, style]} {...rest}>
+      {children || text}
+    </Text>
+  );
 }
--- src/components/Title/styles.ts
+++ src/components/Title/styles.ts
@@ -3,7 +3,6 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   title: {
-    marginBottom: Theme.SPACING.x3,
     fontFamily: Theme.FONTS.TITLE,
     fontSize: 24,
     lineHeight: 29,
